52940c33599fb0ab60c34bd80cf92975ef678e5d
abd03mg <eemgyg@gmail.com>
2024-04-01 06:31:35 +0200

    fds

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,8 +1,7 @@
 #install and configure nginx using puppet.
 exec { 'command':
-  command  => 'apt-get -y update;
-  apt-get -y install nginx;
-  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
-  service nginx restart',
+  command  => 'apt -y update;
+apt -y install nginx;
+sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;service nginx restart',
   provider => shell,
 }
